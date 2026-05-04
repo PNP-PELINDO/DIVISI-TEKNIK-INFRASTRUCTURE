1d5cbe53bfe5221fcb0ede06f15fc1217fa0f688
Kardiko Anando <142975859+TRKA234@users.noreply.github.com>
2026-05-04 16:39:45 +0700

    feat: initialize landing page layout with custom branding and filtering interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>Portal Infrastruktur | Pelindo Regional Group</title>
+    <link rel="icon" href="{{ asset('logo_pelindo.png') }}">
     
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
